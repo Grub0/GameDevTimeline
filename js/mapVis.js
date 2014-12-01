a5270e2d2864ba849909c771a792369c025a84df
unknown <rjc9008@OH1380-11.main.ad.rit.edu>
2014-12-01 14:50:48 -0500

    Starting table, needs to be styled and filled in

--- js/mapVis.js
+++ js/mapVis.js
@@ -45,7 +45,6 @@ d3.csv("data/video_game_developers.csv", function(error, data) {
             }
         }
         createMap();
-
         // Default points
         update(1960);
     }
@@ -77,6 +76,8 @@ function createMap() {
     });
     animating = true;
     startAnimation();
+	var informationTable = tabulate(dataset, ["company","city"]);
+
 }
 
 function createZoomedMap() {
@@ -188,4 +189,41 @@ function update(year)
     d3.select("#count").text(yearDataset.length);
     // Update the points
     createPoints(yearDataset);
+}
+
+// The table generation function
+function tabulate(data, columns) {
+    var table = d3.select("body").append("table")
+            .attr("style", "margin-left: 250px"),
+        thead = table.append("thead"),
+        tbody = table.append("tbody");
+
+    // append the header row
+    thead.append("tr")
+        .selectAll("th")
+        .data(columns)
+        .enter()
+        .append("th")
+            .text(function(column) { return column; });
+
+    // create a row for each object in the data
+    var rows = tbody.selectAll("tr")
+        .data(data)
+        .enter()
+        .append("tr");
+
+    // create a cell in each row for each column
+    var cells = rows.selectAll("td")
+        .data(function(row) {
+            return columns.map(function(column) {
+                return {column: column, value: row[column]};
+            });
+        })
+        .enter()
+        .append("td")
+		.style("fill", "white")
+        .attr("style", "font-family: Courier") // sets the font style
+        .html(function(d) { return d.value; });
+    
+    return table;
 }
