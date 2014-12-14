c6745990956c4409455b055c86709f4b1c94cb94
mia-hlynchuk <solomiya.hlynchuk@gmail.com>
2014-12-13 19:32:18 -0500

    Populated each panel with a table

--- js/mapVis.js
+++ js/mapVis.js
@@ -27,6 +27,13 @@ var mapLegend = [
     {id:"multipleCategories", key:"Multiple Categories", color:"#FFF"}
 ];
 
+var colTableData = [
+    {dataText:"Company", dataField:"company"},
+    {dataText:"City", dataField:"city"},
+    {dataText:"Country", dataField:"country"},
+    {dataText:"Year Established", dataField:"yearEST"},
+    {dataText:"Category", dataField:"category"}
+];
 
 d3.json("data/video_game_developers.json", function(error, data) {
 
@@ -139,26 +146,34 @@ function createMap() {
 
 // Draws the coordinate points
 function createPoints(data) {
-    //console.log("length: "  + data);
+    //console.log("data: "  + data);
     // Create the points 
-    map.bubbles(data, {
+    var points = [];
+    for(var key in data){
+        for(var i = 0; i < data[key].length; i++){
+            points.push(data[key][i]);
+        }
+    }
+
+    map.bubbles(points, {
         borderWidth: 0,
         fillOpacity: 1,
-        popupTemplate: function(data) {
+        popupTemplate: function(d) {
             // Info box 
             var string =  '<div class="popover top>';
-            string += '<div class="arrow"></div><h3 class="popover-title">' + data.company + '</h3>';
-            string += '<div class="popover-content"><p>Located: ' + data.city + ", " + data.country;
-            string += "<br/>Founded: " + data.yearEST;
-            if(data.yearClosed != '0') {
-                string += "<br/>Closed: " + data.yearClosed; 
+            string += '<div class="arrow"></div><h3 class="popover-title">' + d.company + '</h3>';
+            string += '<div class="popover-content"><p>Located: ' + d.city + ", " + d.country;
+            string += "<br/>Founded: " + d.yearEST;
+            if(d.yearClosed != '0') {
+                string += "<br/>Closed: " + d.yearClosed; 
             }
-            string += "<br/>Category: " + data.category;
+            string += "<br/>Category: " + d.category;
             string += '</p></div></div>';
 
             return string;
         }
     });
+    activateComponents();
 }
     
 function clearBox(elementID) {
@@ -193,7 +208,7 @@ function startAnimation() {
 }
 
 // update the elements
-function update(year, type, data) {
+function update(year, categoryType) {
     // adjust the text on the range slider
     d3.select("#year-value").text(year);
     d3.select("#year").property("value", year);
@@ -201,115 +216,138 @@ function update(year, type, data) {
 
     d3.select("#titleHeader").text("Game Companies in " + yearShown);
 
-    // Temporary array to hold specific points based on the year
-    var yearDataset = [];
+    // Temporary object to hold specific points based on the year
+    var yearDataset = {};
 
-    // Filter out the points based on the year
-    if(type == "allCategory"){
+    // If its all categories it needs to go through a for each loop
+    if(categoryType == "allCategory") {
         for(var key in dataset){
+            // Add a cateogry key to yearDataset object, 
+            // and make it an empty array
+            yearDataset[key] = [];
+
+            // In the main object fileter out the current and previous years, 
+            // and push it to the new cateogry array
             for(var i = 0; i < dataset[key].length; i++){
-                if(parseInt(dataset[key][i].yearEST) <= year && (parseInt(dataset[key][i].yearClosed) > year || parseInt(dataset[key][i].yearClosed) == 0)) 
-                {
-                    yearDataset.push(dataset[key][i]);
-                }
+                if(parseInt(dataset[key][i].yearEST) <= year && (parseInt(dataset[key][i].yearClosed) > year || parseInt(dataset[key][i].yearClosed) == 0))
+                    yearDataset[key].push( dataset[key][i] );
             }
         }  
-    }else if(type == "category"){
-        for(var i = 0; i < data.length; i++){
-                if(parseInt(data[i].yearEST) <= year && (parseInt(data[i].yearClosed) > year || parseInt(data[i].yearClosed) == 0)) 
-                {
-                    yearDataset.push(data[i]);
-                }
-            }
     }
-    
+    else { 
+        // Since we know the category, there is no need for a for each loop
+        yearDataset[categoryType] = [];
+
+        var catArray = dataset[categoryType]; 
+        for(var i = 0; i < catArray.length; i++){
+            if(parseInt(catArray[i].yearEST) <= year && (parseInt(catArray[i].yearClosed) > year || parseInt(catArray[i].yearClosed) == 0)) 
+                yearDataset[categoryType].push( catArray[i] );
+        }
+    }
+
     // Update the #count
-    d3.select("#count").text(yearDataset.length);
+   // d3.select("#count").text(yearDataset.length);
+
     // Update the points
     createPoints(yearDataset);
+
 	//Update the Table
     if(animating == false) {
 	   clearBox('dataTable');
-	   var informationTable = tabulate(yearDataset, ["Company","City","Country","Year Established","Category"]);
+	   var informationTable = tabulate(yearDataset);
     }
     makeChart(yearShown);
 }
 
 // The table generation function
-function tabulate(data, columns) {
-    /*var table = d3.select("#dataTable").append("table").attr("class","table")
-        thead = table.append("thead"),
-        tbody = table.append("tbody");
+function tabulate(data) {
+    console.log(data);
+    var tablesHolder = d3.select("#dataTable");
 
-    // append the header row
-    thead.append("tr")
-        .selectAll("th")
-        .data(columns)
-        .enter()
-        .append("th")
-            .text(function(column) { return column; });
-
-    // create a row for each object in the data
-    var rows = tbody.selectAll("tr")
-        .data(data)
-        .enter()
-        .append("tr");
-
-    // create a cell in each row for each column
-    var cells = rows.selectAll("td")
-        .data(function(row) {
-            return columns.map(function(column) {
-                return {column: column, value: row[column]};
-            });
-        })
-        .enter()
-        .append("td")
-        //.attr("style", "font-family: Courier") // sets the font style
-        .html(function(d) { return d.value; });
-    
-    return table;*/
-    var table = d3.select("#dataTable");
-    var panel = table.selectAll("div")
-        .data(mapLegend)
-        .enter()
-        .append("div")
-            .attr("id", "accordion")
-            .attr("class","panel-group")
-            .attr("role", "tablist")
-            .attr("aria-multiselectable", "true")
-            .append("div")
+    for(var cat in data){
+        if(data[cat].length > 0){
+            var panel = tablesHolder.append("div")
+                .attr("id", "accordion")
+                .attr("class","panel-group")
+                .attr("role", "tablist")
+                .attr("aria-multiselectable", "true")
+                .append("div")
                 .attr("class", "panel panel-default");
 
-    // Panel Heading
-    panel.append("div")
-        .attr("id", function(d, i){ return d.key; })
-        .attr("class", "panel-heading")
-        .attr("role", "tab")
-        .style("background-color", function(d, i){ return d.color; })
-        .append("h4")
-            .attr("class", "panel-title")
-            .append("a")
-                .attr("data-toggle", "collapse")
-                .attr("data-parent","#accordion")
-                .attr("href", function(d, i){ return "#collapse" + i; })
-                .attr("aria-expanded", "false")
-                .attr("aria-controls", function(d, i){ return "collapse" + i; })
-                .text(function(d){ return d.key; });
+            // Panel Heading
+            var panHeading = panel.append("div")
+                .attr("id", cat + "-panel")
+                .attr("class", "panel-heading")
+                .attr("role", "tab")
+                .style("background-color", function(){ 
+                    for(var i =0; i < mapLegend.length; i++){
+                        if(cat == mapLegend[i].id) return mapLegend[i].color;
+                    }
+                })
+                .append("h4")
+                    .attr("class", "panel-title")
+                    .append("a")
+                        .attr("data-toggle", "collapse")
+                        .attr("data-parent","#accordion")
+                        .attr("href", function(){ 
+                            for(var i =0; i < mapLegend.length; i++){
+                                if(cat == mapLegend[i].id) return "#collapse" + i;
+                            }
+                        })
+                        .attr("aria-expanded", "false")
+                        .attr("aria-controls", function(){ 
+                            for(var i =0; i < mapLegend.length; i++){
+                                if(cat == mapLegend[i].id) return "collapse" + i;
+                            }
+                        })
+                        .text(function(){ 
+                            for(var i =0; i < mapLegend.length; i++){
+                                if(cat == mapLegend[i].id) return mapLegend[i].key;
+                            }
+                        });
     
-    // Panel Body
-    panel.append("div")
-        .attr("id", function(d, i){ return "collapse" + i; })
-        .attr("class", "panel-collapse collapse")
-        .attr("role", "tabpanel")
-        .attr("aria-labelledby", function(d, i){ return "heading"+i; })
-        .append("div")
-            .attr("class", "panel-body")
-            .text(function(d){ return "All the " + d.key + " companies will go here."});
+            // Panel Body
+            var panBody = panel.append("div")
+                .attr("id", function(){ 
+                    for(var i =0; i < mapLegend.length; i++){
+                        if(cat == mapLegend[i].id) return "collapse" + i;
+                    }
+                })
+                .attr("class", "panel-collapse collapse")
+                .attr("role", "tabpanel")
+                .attr("aria-labelledby", function(){ 
+                    for(var i =0; i < mapLegend.length; i++){
+                        if(cat == mapLegend[i].id) return "heading" + i; 
+                    }
+                })
+                .append("div")
+                    .attr("class", "panel-body");
+
+            // Create table
+            panBody.append("table")
+                .attr("id", function(){ return cat + "-table"; })
+                .attr("class", function(){ return "category-table"; })
+                .append("thead")
+                    .append("tr")
+                    .selectAll("th")
+                    .data(colTableData)
+                    .enter()
+                        .append("th")
+                            .attr("data-field", function(col){ return col.dataField; })
+                            .text(function(col){ return col.dataText; });
+
+            console.log("after table: "+ cat+"-table" );
+        }
+        // Bootstrap table
+        $("#" + cat + "-table").bootstrapTable({
+            data: data[cat]
+        }); 
+    }  
 }
 
+
+
 function createLegend() {
-    console.log("from legend:");
-    console.log(dataset);
     var key = d3.select("#legend");
 
     // Create div elements for colors
@@ -339,6 +377,6 @@ function activateComponents() {
         .on("click", function(){
             // Get the categoy id
             var category = this.id;
-            update(yearShown,"category", dataset[category]);
+            update(yearShown, category); //, dataset[category]);
         });
 }
